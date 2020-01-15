32f30636d541c3825ed687d5eb40f2b9f005c1c8
yuzhanglong <yuzl1123@163.com>
2020-01-15 14:58:50 +0800

    封装echarts部分组件

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 </template>
 
 <script>
-
   export default {
     name: 'app',
     components: {},
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
+import echarts from './utils/echarts';
 
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(echarts);
 
 
 new Vue({
