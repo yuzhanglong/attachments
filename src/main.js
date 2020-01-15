f4a59da06c528222ac93b50c7d74a0d383cca86d
yuzhanglong <yuzl1123@163.com>
2020-01-15 12:48:51 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
---
+++ src/network/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://127.0.0.1:5000',
+    timeout: 5000,
+  });
+  // 添加请求拦截器
+  instance.interceptors.request.use(config => {
+    // 在发送请求之前做些什么
+    return config;
+  }, error => {
+    // 对请求错误做些什么
+    return error;
+  });
+
+  // 添加响应拦截器
+  instance.interceptors.response.use(res => {
+    // 对响应数据做点什么
+    return res.data;
+  }, error => {
+    // 对响应错误做点什么
+    return Promise.reject(error.response.data);
+  });
+  return instance(config);
+}
