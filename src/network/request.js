c3b9b1611df481192a3abd7d04db77044b06ae85
yuzhanglong <yuzl1123@163.com>
2020-02-16 12:12:08 +0800

    设置localhost

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import {hideLoading, showLoading} from "../utils/loading";
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://192.168.0.129:5000',
+    baseURL: 'http://127.0.0.1:5000/',
     timeout: 5000,
   });
   // 添加请求拦截器
