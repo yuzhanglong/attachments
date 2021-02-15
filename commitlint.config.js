715879be2918fb0b4f4eaa879f713dbb5397f62f
coderyzl <yuzl1123@163.com>
2021-02-15 17:47:19 +0800

    :construction_worker: ci: 更新 commitLint 流程，并为 commit 增加 emoji

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
+/*
+ * File: commitlint.config.js
+ * Description: commitlint 配置
+ * Created: 2021-2-15 16:37:31
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
 module.exports = {
-  extends: ['@commitlint/config-angular']
+  extends: ['@commitlint/config-conventional']
 }
