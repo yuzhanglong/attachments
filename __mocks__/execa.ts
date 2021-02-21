60fba0b33b6e942322dc99555111105bff32e8f1
coderyzl <yuzl1123@163.com>
2021-02-21 19:41:51 +0800

    :white_check_mark: test: 增加 io 相关代码的单元测试 mocks

---
+++ __mocks__/execa.ts
@@ -0,0 +1,13 @@
+/*
+ * File: execa.ts
+ * Description: execa mock
+ * Created: 2021-2-21 19:37:11
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
+const execa = (command: string) => {
+  console.log(command)
+}
+
+module.exports = execa
