9c2aaccdbc598b90c951720fbeecd57220117cdb
yuzhanglong <yuzl1123@163.com>
2021-02-25 23:26:27 +0800

    :white_check_mark: test: 补充 packageManager、template 单元测试

--- packages/serendipity-public/__tests__/logger.spec.ts
+++ packages/serendipity-public/__tests__/logger.spec.ts
@@ -7,34 +7,39 @@
  */
 
 
-import logger from '../bin/utils/logger'
+import logger from '../src/utils/logger'
 
 describe('logger 测试', () => {
   test('info', () => {
+    logger.info('hello world info')
     logger.info = jest.fn()
     logger.info('hello world info')
     expect(logger.info).toBeCalledWith('hello world info')
   })
 
   test('warn', () => {
+    logger.warn('hello world warn')
     logger.warn = jest.fn()
     logger.warn('hello world warn')
     expect(logger.warn).toBeCalledWith('hello world warn')
   })
 
   test('done', () => {
+    logger.done('hello world done')
     logger.done = jest.fn()
     logger.done('hello world done')
     expect(logger.done).toBeCalledWith('hello world done')
   })
 
   test('error', () => {
+    logger.error('hello world error')
     logger.error = jest.fn()
     logger.error('hello world error')
     expect(logger.error).toBeCalledWith('hello world error')
   })
 
   test('log', () => {
+    logger.log('hello world log')
     logger.log = jest.fn()
     logger.log('hello world log')
     expect(logger.log).toBeCalledWith('hello world log')
--- packages/serendipity-public/__tests__/packageManager.spec.ts
+++ packages/serendipity-public/__tests__/packageManager.spec.ts
@@ -6,7 +6,13 @@
  * Email: yuzl1123@163.com
  */
 
-import { PackageManager } from '@attachments/serendipity-public'
+import * as fs from 'fs'
+import { patchRequire } from 'fs-monkey'
+import { PackageManager } from '../src'
+
+const mockedExeca = require('../../../__mocks__/execa')
+
+jest.mock('execa')
 
 describe('packageManager 测试模块', () => {
   test('模块安装字符串生成', () => {
@@ -23,7 +29,46 @@ describe('packageManager 测试模块', () => {
     })).toStrictEqual('yarn add foo@1.0.0')
 
     expect(packageManager.getInstallCommand({
-      name: 'foo',
+      name: 'foo'
     })).toStrictEqual('yarn add foo')
+  })
+
+  test('从文件系统中获取 package 配置', () => {
+    fs.writeFileSync('/package.json', '{"version": "1.0.0"}')
+    patchRequire(fs)
+    const pm = PackageManager.createWithResolve('/')
+    expect(pm.getPackageConfig()).toStrictEqual({
+      'version': '1.0.0'
+    })
+  })
+
+  test('配置合并', () => {
+    const pm = new PackageManager('/')
+    pm.mergeIntoCurrent({
+      'version': '1.0.0',
+      'dependencies': {
+        'foo': '1.0.0'
+      },
+      'devDependencies': {
+        'bar': '1.0.0'
+      }
+    })
+    expect(pm.getPackageConfig()).toStrictEqual({
+      'version': '1.0.0',
+      'dependencies': {
+        'foo': '1.0.0'
+      },
+      'devDependencies': {
+        'bar': '1.0.0'
+      }
+    })
+  })
+
+  test('尝试安装相关依赖', async () => {
+    const pm = new PackageManager('/')
+    await pm.installDependencies()
+    expect(mockedExeca.getCommands()).toStrictEqual([
+      'yarn install'
+    ])
   })
 })
