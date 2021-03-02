7605d7a029d6b24df8e718b958a213ed9123c636
yuzhanglong <yuzl1123@163.com>
2021-03-02 15:05:13 +0800

    :white_check_mark: test: 为 presetManager 补充单元测试

---
+++ packages/serendipity/__tests__/presetManager.spec.ts
@@ -0,0 +1,128 @@
+/*
+ * File: presetManager.spec.ts
+ * Description: preset 单元测试
+ * Created: 2021-3-2 13:27:19
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
+import { generateTempPathInfo } from '@attachments/serendipity-public/bin/utils/testUtils'
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+import PresetManager from '../src/presetManager'
+
+// eslint-disable-next-line max-lines-per-function
+describe('preset Manager 模块测试', () => {
+  const fsHelper = generateTempPathInfo()
+
+  const mock = new MockAdapter(axios)
+
+  mock.onGet('http://mock_preset').reply(200, 'const path = require(\'path\')\n' +
+    '\n' +
+    'module.exports = {\n' +
+    '  plugins: [\n' +
+    '    {\n' +
+    '      name: \'@attachments/serendipity-plugin-react\',\n' +
+    '      path: path.resolve(__dirname, \'../../packages/serendipity-plugin-react\')\n' +
+    '    },\n' +
+    '    {\n' +
+    '      name: \'@attachments/serendipity-plugin-babel\',\n' +
+    '      path: path.resolve(__dirname, \'../../packages/serendipity-plugin-babel\')\n' +
+    '    }\n' +
+    '  ]\n' +
+    '}')
+
+  mock.onGet('http://mock_preset_async_fn').reply(200, 'const axios = require("axios")\n' +
+    'module.exports = async () => {\n' +
+    '      const isFooPlugin = false\n' +
+    '      result = await axios.get(\'http://mock_preset\')\n' +
+    '      return {\n' +
+    '        plugins: [\n' +
+    '          {\n' +
+    '            name: isFooPlugin ? \'foo-plugin\' : \'bar-plugin\'\n' +
+    '          }\n' +
+    '        ]\n' +
+    '      }\n' +
+    '    }')
+
+  afterAll(() => {
+    fsHelper.removeDir()
+  })
+
+  test('通过 preset 对象来初始化 preset', () => {
+    const pm = new PresetManager(fsHelper.path)
+    pm.initPresetByObject({
+      plugins: [
+        {
+          name: 'hello-world-plugin'
+        }
+      ]
+    })
+    expect(pm.getPreset()).toStrictEqual({
+      plugins: [
+        {
+          name: 'hello-world-plugin'
+        }
+      ]
+    })
+  })
+
+  test('正确获取应该在构建阶段删除的 plugins', () => {
+    const pm = new PresetManager(fsHelper.path)
+    pm.initPresetByObject({
+      plugins: [
+        {
+          name: 'plugin-1',
+          removeAfterConstruction: true
+        },
+        {
+          name: 'plugin-2',
+          removeAfterConstruction: null
+        },
+        {
+          name: 'plugin-3',
+          removeAfterConstruction: false
+        }
+      ]
+    })
+    expect(pm.getPluginNamesShouldRemove()).toStrictEqual([
+      'plugin-1'
+    ])
+  })
+
+  test('执行函数形式的 preset, 异步环境下', async () => {
+    const pm = new PresetManager(fsHelper.path)
+    await pm.initPresetByUrl('http://mock_preset_async_fn')
+    expect(pm.getPreset()).toStrictEqual({
+      'plugins': [
+        {
+          'name': 'bar-plugin'
+        }
+      ]
+    })
+  })
+
+  test('通过网络请求来获取 preset', async () => {
+    const pm = new PresetManager(fsHelper.path)
+    await pm.initPresetByUrl('http://mock_preset')
+    expect(pm.getPreset()).toStrictEqual({
+      'plugins': [
+        {
+          'name': '@attachments/serendipity-plugin-react',
+          'path': 'D:\\projects\\serendipity\\playground\\packages\\serendipity-plugin-react'
+        },
+        {
+          'name': '@attachments/serendipity-plugin-babel',
+          'path': 'D:\\projects\\serendipity\\playground\\packages\\serendipity-plugin-babel'
+        }
+      ]
+    })
+  })
+
+
+  test('preset plugins 不存在时尝试获取构建阶段删除的 plugins', () => {
+    const pm = new PresetManager(fsHelper.path)
+    expect(pm.getPluginNamesShouldRemove()).toStrictEqual([])
+  })
+})
+
