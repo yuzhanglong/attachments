85ea453b78b4331bf3abb7bfc5be69ac94037864
yuzhanglong <yuzl1123@163.com>
2021-03-26 09:55:04 +0800

    :construction_worker: ci: update cnpm sync workflow

---
+++ scripts/cnpm-sync.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 同步 CNPM 镜像
+
+wget https://npm.taobao.org/sync/@attachments/serendipity
+wget https://npm.taobao.org/sync/@attachments/serendipity-core
+wget https://npm.taobao.org/sync/@attachments/serendipity-plugin-babel
+wget https://npm.taobao.org/sync/@attachments/serendipity-plugin-eslint
+wget https://npm.taobao.org/sync/@attachments/serendipity-plugin-init
+wget https://npm.taobao.org/sync/@attachments/serendipity-plugin-react
+wget https://npm.taobao.org/sync/@attachments/serendipity-public
+wget https://npm.taobao.org/sync/@attachments/serendipity-scripts
+wget https://npm.taobao.org/sync/@attachments/serendipity-webpack-plugin
+wget https://npm.taobao.org/sync/@attachments/serendipity-workflows
