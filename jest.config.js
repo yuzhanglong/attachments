e097c3c38ebaecff2c1e43e12c35f19b5afb3a54
yuzhanglong <yuzl1123@163.com>
2021-05-27 15:58:26 +0800

    ci: reset coverage files

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,27 @@ module.exports = {
     'playground',
     'examples'
   ],
-  collectCoverage: true
-  // collectCoverageFrom: [
-  //   'packages/**/*.ts',
-  //   '!**/node_modules/**',
-  //   '!**/templates/**',
-  //   '!**/bin/**'
-  // ]
+  collectCoverage: true,
+  collectCoverageFrom: [
+    // packages 子包
+    'packages/serendipity-public/**/*.ts',
+    'packages/serendipity-core/**/*.ts',
+
+    // 第三方包文件
+    '!**/node_modules/**',
+
+    // 插件模板
+    '!**/templates/**',
+
+    // 忽略打包产物
+    '!**/bin/**',
+    '!**/esm/**',
+    '!**/lib/**',
+
+    // 命令行模块不考虑
+    '!packages/serendipity-cli/**/*.ts'
+
+    // 插件的测试暂时忽略，未来会单独开一个插件测试的包
+  ]
 }
 
