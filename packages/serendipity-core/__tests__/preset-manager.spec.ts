113d5f6c683289167a5023aba796342ec768f237
yuzhanglong <yuzl1123@163.com>
2021-05-28 21:56:09 +0800

    test: refactor some test

---
+++ packages/serendipity-core/__tests__/preset-manager.spec.ts
@@ -0,0 +1,165 @@
+/*
+ * File: presetManager.spec.ts
+ * Description: preset 单元测试
+ * Created: 2021-3-2 13:27:19
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+import { PRESET_CDN_BASE_URL } from '@attachments/serendipity-public'
+import { PresetManager } from '../src/preset-manager'
+
+// eslint-disable-next-line max-lines-per-function
+describe('preset Manager 模块测试', () => {
+  const mock = new MockAdapter(axios)
+
+  mock.onGet('https://mock_preset').reply(200, 'const path = require(\'path\')\n' +
+    '\n' +
+    'module.exports = {\n' +
+    '  plugins: [\n' +
+    '    {\n' +
+    '      name: \'@attachments/serendipity-plugin-react\',\n' +
+    '    },\n' +
+    '    {\n' +
+    '      name: \'@attachments/serendipity-plugin-babel\',\n' +
+    '    }\n' +
+    '  ]\n' +
+    '}')
+
+  mock.onGet('https://mock_preset').reply(200, 'const path = require(\'path\')\n' +
+    '\n' +
+    'module.exports = {\n' +
+    '  plugins: [\n' +
+    '    {\n' +
+    '      name: \'@attachments/serendipity-plugin-react\',\n' +
+    '    },\n' +
+    '    {\n' +
+    '      name: \'@attachments/serendipity-plugin-babel\',\n' +
+    '    }\n' +
+    '  ]\n' +
+    '}')
+
+  mock.onGet(`${PRESET_CDN_BASE_URL}/react.js`).reply(200, 'const path = require(\'path\')\n' +
+    '\n' +
+    'module.exports = {\n' +
+    '  plugins: [\n' +
+    '    {\n' +
+    '      name: \'@attachments/foo\',\n' +
+    '    },\n' +
+    '    {\n' +
+    '      name: \'@attachments/bar\',\n' +
+    '    }\n' +
+    '  ]\n' +
+    '}')
+
+
+  mock.onGet('https://mock_preset_async_fn').reply(200, 'const axios = require("axios")\n' +
+    'module.exports = async () => {\n' +
+    '      const isFooPlugin = false\n' +
+    '      return {\n' +
+    '        plugins: [\n' +
+    '          {\n' +
+    '            name: isFooPlugin ? \'foo-plugin\' : \'bar-plugin\'\n' +
+    '          }\n' +
+    '        ]\n' +
+    '      }\n' +
+    '    }')
+
+  test('通过 preset 对象来初始化 preset', () => {
+    const pm = new PresetManager(fsHelper.path)
+    pm.createPresetManagerByObject({
+      plugins: [
+        {
+          name: 'hello-world-plugin'
+        }
+      ]
+    })
+    expect(pm.getPreset()).toStrictEqual({
+      plugins: [
+        {
+          name: 'hello-world-plugin'
+        }
+      ]
+    })
+  })
+
+  test('正确获取应该在构建阶段删除的 plugins', () => {
+    const pm = new PresetManager(fsHelper.path)
+    pm.createPresetManagerByObject({
+      plugins: [
+        {
+          name: 'plugin-1',
+          removeAfterConstruction: true
+        },
+        {
+          name: 'plugin-2',
+          removeAfterConstruction: null
+        },
+        {
+          name: 'plugin-3',
+          removeAfterConstruction: false
+        }
+      ]
+    })
+    expect(pm.getPluginNamesShouldRemove()).toStrictEqual([
+      'plugin-1'
+    ])
+  })
+
+  test('在异步环境下执行函数形式的 preset，执行之，并将其返回值作为 preset 结果', async () => {
+    const pm = new PresetManager(fsHelper.path)
+    await pm.initPresetByUrl('https://mock_preset_async_fn')
+    expect(pm.getPreset()).toStrictEqual({
+      'initialDir': true,
+      'initialDirDefaultName': 'hello-serendipity',
+      'plugins': [
+        {
+          'name': 'bar-plugin'
+        }
+      ]
+    })
+  })
+
+  test('通过网络请求来获取 preset', async () => {
+    const pm = new PresetManager(fsHelper.path)
+    await pm.initPresetByUrl('https://mock_preset')
+    expect(pm.getPreset()).toStrictEqual({
+      'initialDir': true,
+      'initialDirDefaultName': 'hello-serendipity',
+      'plugins': [
+        {
+          'name': '@attachments/serendipity-plugin-react'
+        },
+        {
+          'name': '@attachments/serendipity-plugin-babel'
+        }
+      ]
+    })
+  })
+
+
+  test('preset plugins 不存在时尝试获取构建阶段删除的 plugins', () => {
+    const pm = new PresetManager(fsHelper.path)
+    expect(pm.getPluginNamesShouldRemove()).toStrictEqual([])
+  })
+
+  test('当 preset 传入的不是一个本地路径，也不是一个 http URL 我们会从 GitHub 仓库的默认 preset 获取', async () => {
+    const pm = new PresetManager(fsHelper.path)
+    await pm.initPresetByUrl('react')
+    expect(pm.getPreset()).toStrictEqual({
+      'initialDir': true,
+      'initialDirDefaultName': 'hello-serendipity',
+      'plugins': [
+        {
+          'name': '@attachments/foo'
+        },
+        {
+          'name': '@attachments/bar'
+        }
+      ]
+    })
+  })
+})
+
---
+++ packages/serendipity-core/src/preset-manager.ts
@@ -0,0 +1,124 @@
+/*
+ * File: presetManager.ts
+ * Description: 预设管理类
+ * Created: 2021-2-24 22:12:04
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
+import * as path from 'path'
+import axios from 'axios'
+import {
+  logger, writeFilePromise,
+  DEFAULT_PRESET_NAME,
+  BaseObject
+} from '@attachments/serendipity-public'
+import { SerendipityPreset } from './types/preset'
+import { DEFAULT_PRESET } from './common'
+
+export class PresetManager {
+  private readonly preset: SerendipityPreset
+
+  constructor(preset?: SerendipityPreset) {
+    this.preset = preset || DEFAULT_PRESET
+  }
+
+  /**
+   * 直接使用 preset object 来初始化项目 preset
+   *
+   * @author yuzhanglong
+   * @param preset 对象
+   * @date 2021-5-28 21:45:15
+   */
+  public static async createPresetManagerByObject(preset: BaseObject) {
+    const p = await PresetManager.initPresetObject(preset);
+    return new PresetManager(p);
+  }
+
+  /**
+   * 根据远程路径获取 presetManager
+   *
+   * @author yuzhanglong
+   * @date 2021-5-28 21:27:01
+   * @param basePath preset 保存的临时目录
+   * @param url preset 远程路径
+   */
+  public static async createPresetManagerByRemotePath(basePath: string, url: string) {
+    // 判断是否为远程路径
+    const isRemotePath = url.startsWith('http://') || url.startsWith('https://')
+    if (!isRemotePath) {
+      throw new Error('不合法的 preset 路径!')
+    }
+
+    logger.info(`⚙ 从 ${url} 获取 preset 中...`)
+
+    // 远程获取 preset 内容
+    const response = await axios.get(url)
+    const targetPath = path.resolve(
+      basePath,
+      `${DEFAULT_PRESET_NAME}-${new Date().getTime().toString()}.js`
+    )
+    await writeFilePromise(targetPath, response.data)
+    const presetTmp = require(targetPath)
+    const finalPreset = await PresetManager.initPresetObject(presetTmp) as SerendipityPreset
+    return new PresetManager(finalPreset)
+  }
+
+  /**
+   * 根据本地路径获取 presetManager
+   *
+   * @author yuzhanglong
+   * @date 2021-5-28 21:41:14
+   * @param localPath preset 本地路径
+   */
+  public static async createPresetManagerByLocalPath(localPath: string) {
+    const isLocalPath = localPath.startsWith('/') || (localPath.match(/[a-zA-Z]:(\\\\)|(\/\/)/) !== null)
+    if (!isLocalPath) {
+      throw new Error('不合法本地 preset 路径！')
+    }
+    const res = require(localPath)
+    const finalPreset = await PresetManager.initPresetObject(res)
+    return new PresetManager(finalPreset as SerendipityPreset)
+  }
+
+  /**
+   * 处理 preset，由于 preset 可能是个函数，如果是的话，我们需要执行值以获取最终 preset
+   *
+   * @author yuzhanglong
+   * @date 2021-5-28 21:29:17
+   * @param presetTmp 未处理过的 preset
+   */
+  public static async initPresetObject(presetTmp: BaseObject): Promise<SerendipityPreset> {
+    if (typeof presetTmp === 'function') {
+      const p = await (presetTmp as any)()
+      return {
+        ...DEFAULT_PRESET,
+        ...p as BaseObject
+      }
+    }
+  }
+
+  /**
+   * preset getter
+   *
+   * @author yuzhanglong
+   * @date 2021-2-24 22:24:13
+   */
+  public getPreset() {
+    return this.preset
+  }
+
+  /**
+   * 获取所有需要在构建结束之后删除的 plugins
+   *
+   * @author yuzhanglong
+   * @date 2021-2-26 23:45:24
+   */
+  public getPluginNamesShouldRemove(): string[] {
+    if (Array.isArray(this.preset?.plugins)) {
+      const data = this.preset?.plugins?.filter(res => res.removeAfterConstruction)
+      return data.map(res => res.name)
+    }
+    return []
+  }
+}
