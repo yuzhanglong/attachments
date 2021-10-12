e91ef9f9ef7678577d7aebe6662f3b09be10d151
yuzhanglong <yuzl1123@163.com>
2021-10-12 15:46:59 +0800

    feat: mf-scripts support upgrade command

--- packages/module-federation-toolkits/src/node/mf-scripts.ts
+++ packages/module-federation-toolkits/src/node/mf-scripts.ts
@@ -3,11 +3,13 @@
 import { program } from 'commander';
 import { loadTsConfigFile } from '@attachments/utils/lib/node';
 import * as path from 'path';
+import ncu from 'npm-check-updates';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
 import { getMicroAppWebpackConfig, MicroAppWebpackConfigOptions } from './get-micro-app-webpack-config';
 import { webpackBuild, webpackServe } from './webpack-command';
 import { MicroAppConfig } from './micro-fe-app-config';
 import { generateMfExposeDeclaration } from './generate-mf-expose-declaration';
-
 
 const APP_TYPES: (MicroAppWebpackConfigOptions['type'])[] = ['micro-app', 'base-app'];
 
@@ -76,6 +78,61 @@ program
       const { appConfigPath } = opt;
       const appConfig = await loadTsConfigFile(path.resolve(process.cwd(), appConfigPath));
       await generateMfExposeDeclaration(appConfig as MicroAppConfig);
+    },
+  );
+
+
+// 脚手架包升级
+program
+  .command('upgrade')
+  .description('upgrade mf-lite to newest version')
+  .action(
+    async () => {
+      console.log('[mf-lite] upgrading your mf-lite toolkits dependencies...');
+
+      const DEPS_TO_UPGRADE = [
+        '@attachments/proxy',
+        '@attachments/module-federation-toolkits',
+        '@attachments/utils',
+      ];
+
+      const res = await ncu.run({
+        packageFile: path.resolve(process.cwd(), 'package.json'),
+        upgrade: false,
+        filter: DEPS_TO_UPGRADE,
+      });
+
+      const entries = Object.entries(res);
+
+      // 相关依赖没有需要更新的
+      if (!entries || entries.length === 0) {
+        console.log('\n[mf-lite] mf-lite is already the latest version~');
+        return;
+      }
+
+      for (const [k, v] of entries) {
+        console.log(`${k} => ${chalk.blue(v)}`);
+      }
+
+      const userAnswers = await inquirer.prompt(
+        [
+          {
+            type: 'confirm',
+            message: '[mf-lite] There is a new version of mf-lite, the dependencies information is listed above, are you sure you want to update it?',
+            name: 'shouldUpgrade',
+            default: false,
+          },
+        ],
+      );
+
+      if (userAnswers) {
+        await ncu.run({
+          packageFile: path.resolve(process.cwd(), 'package.json'),
+          upgrade: true,
+          filter: DEPS_TO_UPGRADE,
+        });
+        console.log('[mf-lite] package.json is updated successfully, please run `yarn install` or `npm install` to upgrade your dependencies!');
+      }
     },
   );
 
