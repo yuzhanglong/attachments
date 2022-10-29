81f55351515e2730faeb2abddf020563cc7d7155
yuzhanglong <loveyzl1123@gmail.com>
2022-10-30 00:17:45 +0800

    feat: add react eslint config

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -3,10 +3,12 @@ import type { Linter } from 'eslint';
 module.exports = {
   extends: [
     '@antfu',
+    '@antfu/eslint-config-react',
   ],
   ignorePatterns: ['lib', 'esm', 'cjs'],
   rules: {
     '@typescript-eslint/semi': ['error', 'always'],
     '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
   },
 } as Linter.Config;
